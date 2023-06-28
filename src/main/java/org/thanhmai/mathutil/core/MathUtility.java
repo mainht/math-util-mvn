1855f679bd58d3bc743afaf7a1c35741267ee9f2
mainht <112955345+mainht@users.noreply.github.com>
2023-06-28 11:34:34 +0700

    add screenshot and update source using recursion

--- src/main/java/org/thanhmai/mathutil/core/MathUtility.java
+++ src/main/java/org/thanhmai/mathutil/core/MathUtility.java
@@ -23,20 +23,34 @@ public class MathUtility {
     // chi tinh giai thau tu 0..20
     // >=21! nem exception
     // 0! = 1! = 1
-    public static long getFactorial (int n) {
+//    public static long getFactorial (int n) {
+//        
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be between 0...20, please.");
+//        
+//        if (n == 0 || n ==1)          
+//            return 1;
+//        
+//        long product = 1; //tich nhan don
+//        for (int i = 2; i <= n; i++) 
+//            product *= i;
+//        
+//        return product;
+//        
+//        
+//    }
+        public static long getFactorial (int n) {
         
         if (n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0...20, please.");
         
         if (n == 0 || n ==1)          
             return 1;
-        
-        long product = 1; //tich nhan don
-        for (int i = 2; i <= n; i++) 
-            product *= i;
-        
-        return product;
-        
-        
+        return n * getFactorial(n-1);   
     }
 }
+// 5! = 1.2.3.4.5
+// 5! = 4!      x       5
+// 4! = 4       x 3!
+
+// N! = N x (N-1)!
